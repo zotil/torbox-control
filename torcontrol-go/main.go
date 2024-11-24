129ee12405e0b4c751c58817077e859b461cb5bf
Carlos Martinez <carlosmartinez87@gmail.com>
2024-11-24 09:17:02 -0300

    add circuit example

--- torcontrol-go/main.go
+++ torcontrol-go/main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"torcontrol-go/torcontrol"
 )
 
 func main() {
 	// Initialize the Tor Control Client
-	client, err := torcontrol.NewClient("192.168.42.1", 9051, "CHANGE-IT")
+	client, err := torcontrol.NewClient("192.168.44.1", 9051, "CHANGE-IT")
 	if err != nil {
 		log.Fatalf("Failed to connect to Tor: %v", err)
 	}
@@ -20,12 +19,22 @@ func main() {
 	}(client)
 
 	// Example: Monitor bandwidth events
-	err = client.SetEvents([]string{"BW"})
+	//err = client.SetEvents([]string{"BW"})
+	//if err != nil {
+	//	log.Fatalf("Failed to enable bandwidth events: %v", err)
+	//}
+	//fmt.Println("Monitoring bandwidth events...")
+	//for event := range client.WatchBandwidth() {
+	//	fmt.Printf("Time: %s, Read: %s bytes, Written: %s bytes\n", event.Time, event.Read, event.Written)
+	//}
+
+	// Get circuits and list them
+	circuits, err := client.GetCircuits()
 	if err != nil {
-		log.Fatalf("Failed to enable bandwidth events: %v", err)
+		log.Fatalf("Failed to get circuits: %v", err)
 	}
-	fmt.Println("Monitoring bandwidth events...")
-	for event := range client.WatchBandwidth() {
-		fmt.Printf("Time: %s, Read: %s bytes, Written: %s bytes\n", event.Time, event.Read, event.Written)
+	for _, circuit := range circuits {
+		log.Printf("ID: %s, Status: %s, Path: %s\n", circuit.ID, circuit.Status, circuit.Path)
 	}
+
 }
